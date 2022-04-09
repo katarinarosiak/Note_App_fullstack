b58acf2f75ebce31309b082f3c0d85abbecb2d2f
katarina <katarinarosiak@gmail.com>
2022-04-09 22:47:21 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,87 +1,119 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
+const Note = require('./models/note')
 
+const app = express()
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+// const url = "mongodb+srv://katarinarosiak:MB_jebusyt%40m%40rcelina0811@textcluster.fqjce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// mongoose.connect(url)
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+
+
+
+//database
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(note => {
+    res.json(note);
+  });
+});
+
+//get a note and of it doesn't exist raise an error
+//if there is no note the note is null
+//if the id is malformed findById will trhow an error 
+//then the returned promise will be rejected  and the 
+// catch block will run 
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+//if the note was deleted succesfully 
+// return status 204 no content
+// the result could be used to check if the
+// resurce was deleted 
+app.delete('/api/notes/:id', (request, response, next) => {
+	console.log(request);
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+// By default, the updatedNote parameter of the
+// event handler receives the original document
+// without the modifications. We added the 
+//optional { new: true }parameter, which will cause
+// our event handler to be called with the new modified
+// document instead of the original.
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
